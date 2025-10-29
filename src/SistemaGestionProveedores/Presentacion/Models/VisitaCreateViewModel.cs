059d09b0f6bc2125cea1fcbd2d1cfbb0c7487ae0
Leon Breslauer <leonbreslauer.tuc@gmail.com>
2025-10-29 15:47:21 -0300

    Agrego ViewModels para vista de Crear Visita

---
+++ src/SistemaGestionProveedores/Presentacion/Models/VisitaCreateViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Presentacion.Models;
+
+public class VisitaCreateViewModel
+{
+    public int TecnicoId { get; set; }
+    public List<SelectListItem> Tecnicos { get; set; } = new();
+    public List<SelectListItem> ProductosOfrecidos { get; set; } = new();
+    public List<ProductoVisitaViewModel> ProductosVisita { get; set; } = new();
+    public decimal MontoTotal { get; set; }
+    public string? Observaciones { get; set; }
+}
