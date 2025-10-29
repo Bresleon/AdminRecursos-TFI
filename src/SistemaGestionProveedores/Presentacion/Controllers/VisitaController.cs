ad0e2a40633bfc6cfde0fae6f67bfbace45875b1
Leon Breslauer <leonbreslauer.tuc@gmail.com>
2025-10-29 15:47:48 -0300

    Agrego vista de Crear Visita

--- src/SistemaGestionProveedores/Presentacion/Controllers/VisitaController.cs
+++ src/SistemaGestionProveedores/Presentacion/Controllers/VisitaController.cs
@@ -1,7 +1,9 @@
 ﻿using Dominio.Enums;
 using Infraestructura.Datos;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using Presentacion.Models;
 
 namespace Presentacion.Controllers;
 
@@ -27,8 +29,22 @@ public class VisitasController : Controller
 
     public IActionResult Create()
     {
-        return View();
+        var model = new VisitaCreateViewModel
+        {
+            Tecnicos = _context.Tecnicos
+                .Select(t => new SelectListItem
+                {
+                    Value = t.Id.ToString(),
+                    Text = t.Nombre
+                })
+                .ToList(),
+
+            ProductosVisita = new List<ProductoVisitaViewModel>()
+        };
+
+        return View(model);
     }
+
 
     public IActionResult Edit()
     {
@@ -39,4 +55,63 @@ public class VisitasController : Controller
     {
         return View();
     }
+
+    #region AJAX_CALLS
+
+    [HttpGet]
+    public JsonResult ObtenerTecnicoPorDni(string dni)
+    {
+        if (string.IsNullOrWhiteSpace(dni))
+            return Json(new { success = false, message = "Debe ingresar un DNI." });
+
+        var tecnico = _context.Tecnicos
+            .Include(t => t.Proveedor)
+            .FirstOrDefault(t => t.DNI == dni);
+
+        if (tecnico == null)
+            return Json(new { success = false, message = "No se encontró un técnico con ese DNI." });
+
+        var productos = _context.Productos
+            .Where(p => p.ProveedorId == tecnico.ProveedorId)
+            .Select(p => new
+            {
+                id = p.Id,
+                nombre = p.Nombre
+            })
+            .ToList();
+
+        return Json(new
+        {
+            success = true,
+            tecnicoId = tecnico.Id,
+            tecnicoNombre = $"{tecnico.Nombre} {tecnico.Apellido}",
+            proveedor = tecnico.Proveedor.RazonSocial,
+            productos
+        });
+    }
+
+
+    [HttpGet]
+    public JsonResult ObtenerProductosPorTecnico(Guid tecnicoId)
+    {
+        var tecnico = _context.Tecnicos
+            .Include(t => t.Proveedor)
+            .FirstOrDefault(t => t.Id == tecnicoId);
+
+        if (tecnico == null)
+            return Json(new { productos = new List<object>() });
+
+        var productos = _context.Productos
+            .Where(p => p.ProveedorId == tecnico.ProveedorId)
+            .Select(p => new
+            {
+                id = p.Id,
+                nombre = p.Nombre
+            })
+            .ToList();
+
+        return Json(new { productos });
+    }
+
+    #endregion
 }
