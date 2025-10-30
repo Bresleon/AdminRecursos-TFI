1ec04acd3407f40c4400ca37b3179ddc6b884f76
Leon Breslauer <leonbreslauer.tuc@gmail.com>
2025-10-30 11:37:56 -0300

    Agrego vista de Consulta de Adquisición de Producto

---
+++ src/SistemaGestionProveedores/Presentacion/Models/ProductoConsultaViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Presentacion.Models;
+
+public class ProductoConsultaViewModel
+{
+    public string NumeroSerie { get; set; } = string.Empty;
+    public string Nombre { get; set; } = string.Empty;
+    public string TipoProducto { get; set; } = string.Empty;
+
+    public DateOnly FechaFinGarantia { get; set; }
+    public DateTime FechaAdquisicion { get; set; }
+
+    public string NombreCompletoTecnico { get; set; } = string.Empty;
+    public string DNITecnico { get; set; } = string.Empty;
+}
