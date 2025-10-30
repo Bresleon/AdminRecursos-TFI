001b2b3530adadfe9de2c73450a60710759d2ff6
Leon Breslauer <leonbreslauer.tuc@gmail.com>
2025-10-30 10:38:09 -0300

    Agrego vista Index de Proveedores

---
+++ src/SistemaGestionProveedores/Presentacion/Models/ProveedorViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Presentacion.Models;
+
+public class ProveedorViewModel
+{
+    public Guid Id { get; set; }
+    public string RazonSocial { get; set; } = string.Empty;
+    public string CUIT { get; set; } = string.Empty;
+    public decimal MontoTotalPagado { get; set; } = 0;
+    public double Calificacion { get; set; } = 0;
+}
