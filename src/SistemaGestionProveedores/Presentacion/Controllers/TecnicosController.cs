c641642fe2d71d928f46a3839c97038f974a47c0
Leon Breslauer <leonbreslauer.tuc@gmail.com>
2025-10-31 00:38:17 -0300

    Agrego vista de Técnicos

---
+++ src/SistemaGestionProveedores/Presentacion/Controllers/TecnicosController.cs
@@ -0,0 +1,38 @@
+﻿using Infraestructura.Datos;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Presentacion.Controllers;
+
+public class TecnicosController : Controller
+{
+    private readonly ApplicationDbContext _context;
+
+    public TecnicosController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public IActionResult Index(string? dni = null)
+    {
+        var tecnicos = _context.Tecnicos.AsQueryable();
+
+        if (!string.IsNullOrEmpty(dni))
+        {
+            ViewData["DNI"] = dni;
+            tecnicos = tecnicos.Where(t => t.DNI == dni);
+        }
+
+        var tecnicosVM = tecnicos.Select(t => new Models.TecnicoViewModel
+        {
+            Id = t.Id,
+            NombreCompleto = t.Nombre + " " + t.Apellido,
+            DNI = t.DNI,
+            Telefono = t.Telefono,
+            Proveedor = t.Proveedor.RazonSocial,
+            Calificacion = t.Calificacion
+        });
+
+        return View(tecnicosVM);
+    }
+}
---
+++ src/SistemaGestionProveedores/Presentacion/Models/TecnicoViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Presentacion.Models;
+
+public class TecnicoViewModel
+{
+    public Guid Id { get; set; }
+    public string NombreCompleto { get; set; } = string.Empty;
+    public string DNI { get; set; } = string.Empty;
+    public string Telefono { get; set; } = string.Empty;
+    public string Proveedor { get; set; } = string.Empty;
+    public double Calificacion { get; set; } = 0;
+}
