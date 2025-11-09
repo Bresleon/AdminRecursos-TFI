8a3b2b7bb1fb7e6c29912398d15744b7c5b37638
Leon Breslauer <leonbreslauer.tuc@gmail.com>
2025-11-09 01:35:14 -0300

    Agrego includes en métodos de IRepositorioGenerico e implemento RepositorioGenerico

--- src/SistemaGestionProveedores/Aplicacion/Interfaces/Repositorios/IRepositorioGenerico.cs
+++ src/SistemaGestionProveedores/Aplicacion/Interfaces/Repositorios/IRepositorioGenerico.cs
@@ -4,10 +4,10 @@ namespace Aplicacion.Interfaces.Repositorios;
 
 public interface IRepositorio<T> where T : class
 {
-    Task<T?> ObtenerPorId(Guid id);
-    Task<T?> Obtener(Expression<Func<T, bool>> filtro);
-    Task<IEnumerable<T>> ObtenerTodos(Expression<Func<T, bool>>? filtro = null);
-    Task<bool> Agregar(T obj);
-    Task<bool> Eliminar(T obj);
-    Task<bool> Modificar(T obj);
+    Task<T?> ObtenerPorId(Guid id, params Expression<Func<T, object>>[] includes);
+    Task<T?> Obtener(Expression<Func<T, bool>> filtro, params Expression<Func<T, object>>[] includes);
+    Task<IEnumerable<T>> ObtenerTodos(Expression<Func<T, bool>>? filtro = null, params Expression<Func<T, object>>[] includes);
+    Task<bool> Agregar(T entidad);
+    Task<bool> Eliminar(T entidad);
+    Task<bool> Modificar(T entidad);
 }
