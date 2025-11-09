63bd2b4879fab4a24bfda0fb86015a166cdd436c
Leon Breslauer <leonbreslauer.tuc@gmail.com>
2025-11-09 02:39:36 -0300

    Agrego interfaz e implementación de servicio y repositorio de Tecnicos

---
+++ src/SistemaGestionProveedores/Aplicacion/Interfaces/Servicios/ITecnicoServicio.cs
@@ -0,0 +1,14 @@
+﻿using Dominio.Entidades;
+using System.Linq.Expressions;
+
+namespace Aplicacion.Interfaces.Servicios;
+
+public interface ITecnicoServicio
+{
+    Task<Tecnico?> Obtener(Guid id);
+    Task<Tecnico?> Obtener(string dni);
+    Task<IEnumerable<Tecnico>> ObtenerTodos(Expression<Func<Tecnico, bool>>? filtro = null);
+    Task Agregar(Tecnico tecnico);
+    Task Modificar(Tecnico tecnico);
+    Task Eliminar(Tecnico tecnico);
+}
---
+++ src/SistemaGestionProveedores/Aplicacion/Servicios/TecnicoServicio.cs
@@ -0,0 +1,65 @@
+﻿using Aplicacion.Interfaces.Repositorios;
+using Aplicacion.Interfaces.Servicios;
+using Dominio.Entidades;
+using System.Linq.Expressions;
+
+namespace Aplicacion.Servicios;
+
+public class TecnicoServicio : ITecnicoServicio
+{
+    private readonly ITecnicoRepositorio _repo;
+
+    public TecnicoServicio(ITecnicoRepositorio repo)
+    {
+        _repo = repo;
+    }
+
+    public async Task Agregar(Tecnico tecnico)
+    {
+        var esExitosa = await _repo.Agregar(tecnico);
+
+        if (!esExitosa)
+            throw new Exception("No se pudo agregar el tecnico");
+    }
+
+    public async Task Eliminar(Tecnico tecnico)
+    {
+        var esExitosa = await _repo.Eliminar(tecnico);
+
+        if (!esExitosa)
+            throw new Exception("No se pudo eliminar el tecnico");
+    }
+
+    public async Task Modificar(Tecnico tecnico)
+    {
+        var esExitosa = await _repo.Modificar(tecnico);
+
+        if (!esExitosa)
+            throw new Exception("No se pudo modificar el tecnico");
+    }
+
+    public async Task<Tecnico?> Obtener(Guid id)
+    {
+        var tecnico = await _repo.ObtenerPorId(id, t => t.Proveedor);
+
+        if (tecnico == null)
+            throw new Exception("No se pudo encontrar el tecnico");
+
+        return tecnico;
+    }
+
+    public async Task<Tecnico?> Obtener(string dni)
+    {
+        var tecnico = await _repo.Obtener(t => t.DNI == dni, t => t.Proveedor);
+
+        if (tecnico == null)
+            throw new Exception("No se pudo encontrar el tecnico");
+
+        return tecnico;
+    }
+
+    public async Task<IEnumerable<Tecnico>> ObtenerTodos(Expression<Func<Tecnico, bool>>? filtro = null)
+    {
+        return await (filtro == null ? _repo.ObtenerTodos() : _repo.ObtenerTodos(filtro));
+    }
+}
