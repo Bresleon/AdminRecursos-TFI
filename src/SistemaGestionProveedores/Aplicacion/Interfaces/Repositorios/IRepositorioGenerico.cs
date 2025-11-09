a9edae54a7a28a700dc2a0bc500eba1c7b6eb78c
Leon Breslauer <leonbreslauer.tuc@gmail.com>
2025-11-09 01:10:16 -0300

    Agrego capa de Aplicacion con interfaces de servicios y repositorios, más implementación de ProveedorServicio

---
+++ src/SistemaGestionProveedores/Aplicacion/Interfaces/Repositorios/IRepositorioGenerico.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq.Expressions;
+
+namespace Aplicacion.Interfaces.Repositorios;
+
+public interface IRepositorio<T> where T : class
+{
+    Task<T?> ObtenerPorId(Guid id);
+    Task<T?> Obtener(Expression<Func<T, bool>> filtro);
+    Task<IEnumerable<T>> ObtenerTodos(Expression<Func<T, bool>>? filtro = null);
+    Task<bool> Agregar(T obj);
+    Task<bool> Eliminar(T obj);
+    Task<bool> Modificar(T obj);
+}
---
+++ src/SistemaGestionProveedores/Aplicacion/Interfaces/Servicios/IProveedorServicio.cs
@@ -0,0 +1,13 @@
+﻿using Dominio.Entidades;
+using System.Linq.Expressions;
+
+namespace Aplicacion.Interfaces.Servicios;
+
+public interface IProveedorServicio
+{
+    Task<Proveedor?> Obtener(Guid id);
+    Task<IEnumerable<Proveedor>> ObtenerTodos(Expression<Func<Proveedor, bool>>? filtro = null);
+    Task Agregar(Proveedor proveedor);
+    Task Modificar(Proveedor proveedor);
+    Task Eliminar(Proveedor proveedor);
+}
