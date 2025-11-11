b29be5ffd39f44aacdd1141ccf55dba7cb28a730
Leon Breslauer <leonbreslauer.tuc@gmail.com>
2025-11-11 00:50:40 -0300

    Agrego interfaces e implementación de servicio y repositorio de Adquisiciones

---
+++ src/SistemaGestionProveedores/Aplicacion/Interfaces/Servicios/IAdquisicionServicio.cs
@@ -0,0 +1,13 @@
+﻿using Dominio.Entidades;
+using System.Linq.Expressions;
+
+namespace Aplicacion.Interfaces.Servicios;
+
+public interface IAdquisicionServicio
+{
+    Task<Adquisicion?> Obtener(Guid id);
+    Task<Adquisicion?> Obtener(string numeroSerie);
+    Task<IEnumerable<Adquisicion>> ObtenerTodos(Expression<Func<Adquisicion, bool>>? filtro = null);
+    Task Agregar(Adquisicion adquisicion);
+    Task Modificar(Adquisicion adquisicion);
+}
